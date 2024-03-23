8576507d24fdcfdd9cd402be8b9d4a5fe39f25ba
agnesm0 <agnese.martini@mail.polimi.it>
2024-03-23 19:32:38 +0100

    class StarterCard

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/StarterCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/StarterCard.java
@@ -1,4 +1,23 @@
 package it.polimi.ingsw.model.cards;
 
+import it.polimi.ingsw.model.cards.enumeration.Resource;
+
+import java.util.ArrayList;
+
 public class StarterCard extends Card{
+    /**
+     * Constructor
+     * @param ID
+     */
+    public StarterCard(int ID){
+        this.ID = ID;
+    }
+
+    /**
+     *
+     * @return an ArrayList with the permanent resources based on the ID of the card
+     */
+    public ArrayList<Resource> getPermanentResources(){
+
+    }
 }
