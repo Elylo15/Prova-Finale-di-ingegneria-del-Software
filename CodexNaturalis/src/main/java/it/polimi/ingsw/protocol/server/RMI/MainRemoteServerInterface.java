02939e2b1c6ee383bf7a13d32e7f7e379db97bf7
Nicolo <nicolo.locatelli@mail.polimi.it>
2024-05-11 12:30:07 +0200

    Preceding with RMI implemementation

--- CodexNaturalis/src/main/java/it/polimi/ingsw/protocol/server/RMI/MainRemoteServerInterface.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/protocol/server/RMI/MainRemoteServerInterface.java
@@ -3,6 +3,10 @@ package it.polimi.ingsw.protocol.server.RMI;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * RMI server that manages client connections and provides methods for clients to establish a new connection with the server.
+ * This is the interface for RMI protocol.
+ */
 public interface MainRemoteServerInterface extends Remote {
     /**
      * It is invoked by a new client that wants to connect to the server.
--- CodexNaturalis/src/main/java/it/polimi/ingsw/protocol/server/RMI/MessageExchanger.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/protocol/server/RMI/MessageExchanger.java
@@ -4,8 +4,25 @@ import it.polimi.ingsw.protocol.messages.Message;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 
 public class MessageExchanger extends UnicastRemoteObject implements MessageExchangerInterface {
+    private Message lastMessage;
+
+    private boolean receiverCanRead;
+    private boolean senderCanWrite;
+
+
+    /**
+     * Constructor for MessageExchanger
+      * @throws RemoteException
+     */
+    public MessageExchanger() throws RemoteException {
+        lastMessage = null;
+        this.receiverCanRead = false;
+        this.senderCanWrite = true;
+    }
+
 
     /**
      * Sends a message to the other side.
@@ -14,7 +31,27 @@ public class MessageExchanger extends UnicastRemoteObject implements MessageExch
      * @throws RemoteException
      */
     @Override
-    public void sendMessage(Message message) throws RemoteException {
+    public synchronized void sendMessage(Message message) throws RemoteException {
+
+        // Wait until the receiver has read
+        while(!senderCanWrite) {
+            try {
+                this.wait();
+            } catch (InterruptedException ignored) {}
+        }
+
+        // Blocks the receiver from reading
+        this.receiverCanRead = false;
+
+        // Overwrites the stored message
+        this.lastMessage = message;
+
+        // Allows the receiver to read
+        this.senderCanWrite = false;
+        this.receiverCanRead = true;
+
+        // Notify the receiver
+        this.notifyAll();
 
     }
 
@@ -25,7 +62,28 @@ public class MessageExchanger extends UnicastRemoteObject implements MessageExch
      * @throws RemoteException
      */
     @Override
-    public Message receiveMessage() throws RemoteException {
-        return null;
+    public synchronized Message receiveMessage() throws RemoteException {
+
+        // Wait until the sender has written
+        while (!receiverCanRead) {
+            try {
+                this.wait();
+            } catch (InterruptedException ignored) {}
+        }
+
+        // Block the sender from writing
+        this.senderCanWrite = false;
+
+        // Obtains the last message
+        Message message = this.lastMessage;
+
+        // Allows the sender to write
+        this.receiverCanRead = false;
+        this.senderCanWrite = true;
+
+        // Notify the sender
+        this.notifyAll();
+
+        return message;
     }
 }
