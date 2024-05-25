51e21ba21faadce4c7ac3af8083bcf7dad4dfaa8
Nicolo <nicolo.locatelli@mail.polimi.it>
2024-05-25 16:38:46 +0200

    New MessageFromGUI

--- CodexNaturalis/src/main/java/it/polimi/ingsw/protocol/client/view/MessageFromGUI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/protocol/client/view/MessageFromGUI.java
@@ -5,22 +5,24 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class MessageFromGUI {
-    private static BlockingQueue<byte[]> storedObjects;
+    private static BlockingQueue<byte[]> storedObjectsToClient;
+    private static BlockingQueue<byte[]> storedObjectsToGUI;
 
 
     /**
      * Constructor for MessageExchanger
      */
     public MessageFromGUI() {
-        storedObjects = new LinkedBlockingQueue<>();
+        storedObjectsToGUI = new LinkedBlockingQueue<>();
+        storedObjectsToClient = new LinkedBlockingQueue<>();
     }
 
     /**
-     * Sends a message to the other side.
+     * Sends a message to Client.
      *
      * @param message Message to be sent.
      */
-    public static void write(Object message) {
+    public static void writeToClient(Object message) {
         try {
             // Serialize the message
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
@@ -28,21 +30,61 @@ public class MessageFromGUI {
             objectOutputStream.writeObject(message);
             byte[] serializedMessage = byteArrayOutputStream.toByteArray();
             // Send the message
-            storedObjects.put(serializedMessage);
+            storedObjectsToClient.put(serializedMessage);
         } catch (IOException | InterruptedException e) {
             Thread.currentThread().interrupt();
         }
     }
 
     /**
-     * Returns the last message received.
+     * Sends a message to GUI.
+     *
+     * @param message Message to be sent.
+     */
+    public static void writeToGUI(Object message) {
+        try {
+            // Serialize the message
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject(message);
+            byte[] serializedMessage = byteArrayOutputStream.toByteArray();
+            // Send the message
+            storedObjectsToGUI.put(serializedMessage);
+        } catch (IOException | InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+
+
+    /**
+     * Returns the last message received for Client.
      *
      * @return Message received.
      */
-    public static Object read() {
+    public static Object readToClient() {
         try {
             // Deserialize the message
-            byte[] serializedMessage = storedObjects.take();
+            byte[] serializedMessage = storedObjectsToClient.take();
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(serializedMessage);
+            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+            // Return the message
+            return objectInputStream.readObject();
+        } catch (IOException | ClassNotFoundException | InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return null;
+        }
+    }
+
+    /**
+     * Returns the last message received for GUI.
+     *
+     * @return Message received.
+     */
+    public static Object readToGUI() {
+        try {
+            // Deserialize the message
+            byte[] serializedMessage = storedObjectsToGUI.take();
             ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(serializedMessage);
             ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
             // Return the message
