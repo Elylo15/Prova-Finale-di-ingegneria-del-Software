0d974c46a75935c11376924785d35863efa94bf6
Nicolo <nicolo.locatelli@mail.polimi.it>
2024-05-25 17:07:25 +0200

    Tests for GUIMessages

---
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/protocol/client/view/GUIMessagesTest.java
@@ -0,0 +1,67 @@
+package it.polimi.ingsw.protocol.client.view;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.Serializable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class GUIMessagesTest {
+
+    @BeforeEach
+    public void setUp() {
+        // Create a new instance of GUIMessages to initialize the queues
+        new GUIMessages();
+    }
+
+    @Test
+    public void testWriteToClientAndReadToClient() throws InterruptedException {
+        String message = "Test message to client";
+
+        // Write the message to the client
+        GUIMessages.writeToClient(message);
+
+        // Read the message from the client
+        Object receivedMessage = GUIMessages.readToClient();
+
+        // Assert that the received message is equal to the sent message
+        assertEquals(message, receivedMessage);
+    }
+
+    @Test
+    public void testWriteToGUIAndReadToGUI() throws InterruptedException {
+        String message = "Test message to GUI";
+
+        // Write the message to the GUI
+        GUIMessages.writeToGUI(message);
+
+        // Read the message from the GUI
+        Object receivedMessage = GUIMessages.readToGUI();
+
+        // Assert that the received message is equal to the sent message
+        assertEquals(message, receivedMessage);
+    }
+
+
+    @Test
+    public void testConcurrentAccess() throws InterruptedException {
+        // Define two different messages for client and GUI
+        String clientMessage = "Client message";
+        String guiMessage = "GUI message";
+
+        // Write the messages to the respective queues
+        GUIMessages.writeToClient(clientMessage);
+        GUIMessages.writeToGUI(guiMessage);
+
+        // Read the messages from the respective queues
+        Object receivedClientMessage = GUIMessages.readToClient();
+        Object receivedGuiMessage = GUIMessages.readToGUI();
+
+        // Assert that the received messages match the sent messages
+        assertEquals(clientMessage, receivedClientMessage);
+        assertEquals(guiMessage, receivedGuiMessage);
+    }
+
+}
